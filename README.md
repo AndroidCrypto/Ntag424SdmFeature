21d5f0385f276866a1858c06d26324dd731fbb65
michaelfehr <michael_fehrgmx.de>
2024-05-17 17:07:42 +0200

    update

--- README.md
+++ README.md
@@ -2,6 +2,56 @@
 
 ## Project Status: Unfinished
 
+
+## Technical information's about NTAG 424 DNA tags
+
+
+
+NTAG 424 DNA datasheet: https://www.nxp.com/docs/en/data-sheet/NT4H2421Tx.pdf
+
+NTAG 424 DNA and NTAG 424 DNA TagTamper features and hints: https://www.nxp.com/docs/en/application-note/AN12196.pdf
+
+### Default content of file 1 (Capability Container)
+
+```plaintext
+Capability Container File
+The Capability Container (CC) file is a StandardData file with respect to access rights
+management and data management. This file will hold the CC-file according to [14]. At
+delivery it will hold following content:
+• CCLEN = 0017h, i.e. 23 bytes
+• T4T_VNo = 20h, i.e. Mapping Version 2.0
+• MLe = 0100h, i.e. 256 bytes
+• MLc = 00FFh, i.e. 255 bytes
+• NDEF-File_Ctrl_TLV
+– T = 04h, indicates the NDEF-File_Ctrl_TLV
+– L = 06h, i.e. 6 bytes
+– NDEF-File File Identifier = E104h
+– NDEF-File File Size = 0100h, i.e. 256 bytes
+– NDEF-File READ Access Condition = 00h, i.e. READ access granted without any security
+– NDEF-File WRITE Access Condition = 00h, i.e. WRITE access granted without any security
+```
+
+This content is written to the  file 1:
+
+```plaintext
+
+byte[] NDEF_FILE_01_CAPABILITY_CONTAINER = Utils.hexStringToByteArray("001720010000ff0406E10401000000");
+
+byte[] NDEF_FILE_01_CAPABILITY_CONTAINER = Utils.hexStringToByteArray("000F20003A00340406E10401000000");
+```
+
+### Default file access rights
+
+|:--:|:-----|:---:|
+| File | Read Access | Write Access |
+
+
+
+
+
+
+## Dependencies
+
 This app is based on the **NXP NTAG 424 DNA Library**, developed by **Jonathan Bartlett**. 
 The full code is available here: https://github.com/johnnyb/ntag424-java. As the library is 
 using a MIT license this app is under the **MIT license** as well.
--- app/src/main/java/de/androidcrypto/ntag424sunfeature/PrepareActivity.java
+++ app/src/main/java/de/androidcrypto/ntag424sunfeature/PrepareActivity.java
@@ -1,14 +1,43 @@
 package de.androidcrypto.ntag424sunfeature;
 
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PrepareActivity extends AppCompatActivity {
+import net.bplearning.ntag424.DnaCommunicator;
+
+import java.io.IOException;
+
+public class PrepareActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback {
+
+    private static final String TAG = PrepareActivity.class.getSimpleName();
+    private com.google.android.material.textfield.TextInputEditText output;
+
+    private DnaCommunicator dnaC = new DnaCommunicator();
+    private NfcAdapter mNfcAdapter;
+    private IsoDep isoDep;
+    private byte[] tagIdByte;
+
+    private byte[] NDEF_FILE_01_CAPABILITY_CONTAINER = Utils.hexStringToByteArray("001720010000ff0406E10401000000");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +49,165 @@ public class PrepareActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+
+        output = findViewById(R.id.etOutput);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    /**
+     * section for UI handling
+     */
+
+    private void writeToUiAppend(TextView textView, String message) {
+        runOnUiThread(() -> {
+            String oldString = textView.getText().toString();
+            if (TextUtils.isEmpty(oldString)) {
+                textView.setText(message);
+            } else {
+                String newString = message + "\n" + oldString;
+                textView.setText(newString);
+                System.out.println(message);
+            }
+        });
+    }
+
+    private void vibrateShort() {
+        // Make a Sound
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(50, 10));
+        } else {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(50);
+        }
+    }
+
+    /**
+     * NFC tag handling section
+     * These methods are running in another thread when a card is discovered and
+     * cannot direct interact with the UI Thread.
+     * Use `runOnUiThread` method to change the UI from these methods
+     */
+
+    @Override
+    public void onTagDiscovered(Tag tag) {
+
+        writeToUiAppend(output, "NFC tag discovered");
+
+        isoDep = null;
+        try {
+            isoDep = IsoDep.get(tag);
+            if (isoDep != null) {
+                // Make a Vibration
+                vibrateShort();
+
+                runOnUiThread(() -> {
+                    output.setText("");
+                });
+
+                isoDep.connect();
+                if (!isoDep.isConnected()) {
+                    writeToUiAppend(output, "Could not connect to the tag, aborted");
+                    isoDep.close();
+                    return;
+                }
+
+                // get tag ID
+                tagIdByte = tag.getId();
+                writeToUiAppend(output, "Tag ID: " + Utils.bytesToHex(tagIdByte));
+                Log.d(TAG, "tag id: " + Utils.bytesToHex(tagIdByte));
+                writeToUiAppend(output, "NFC tag connected");
+
+                runWorker();
+            }
+
+        } catch (IOException e) {
+            writeToUiAppend(output, "ERROR: IOException " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            writeToUiAppend(output, "ERROR: Exception " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null) {
+
+            Bundle options = new Bundle();
+            // Work around for some broken Nfc firmware implementations that poll the card too fast
+            options.putInt(NfcAdapter.EXTRA_READER_PRESENCE_CHECK_DELAY, 250);
+
+            // Enable ReaderMode for all types of card and disable platform sounds
+            // the option NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK is NOT set
+            // to get the data of the tag afer reading
+            mNfcAdapter.enableReaderMode(this,
+                    this,
+                    NfcAdapter.FLAG_READER_NFC_A |
+                            NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK |
+                            NfcAdapter.FLAG_READER_NO_PLATFORM_SOUNDS,
+                    options);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mNfcAdapter != null) {
+            mNfcAdapter.disableReaderMode(this);
+        }
+    }
+
+    private void runWorker() {
+
+        Thread worker = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    dnaC = new DnaCommunicator();
+                    try {
+                        dnaC.setTransceiver((bytesToSend) -> isoDep.transceive(bytesToSend));
+                    } catch (NullPointerException npe) {
+                        writeToUiAppend(output, "Please tap a tag before running any tests, aborted");
+                        return;
+                    }
+                    dnaC.setLogger((info) -> Log.d(TAG, "Communicator: " + info));
+                    dnaC.beginCommunication();
+
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception: " + e.getMessage());
+                    writeToUiAppend(output, "Exception: " + e.getMessage());
+                }
+            }
+        });
+        worker.start();
+    }
+
+    /**
+     * section for options menu
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_return_home, menu);
+
+        MenuItem mReturnHome = menu.findItem(R.id.action_return_home);
+        mReturnHome.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(PrepareActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
