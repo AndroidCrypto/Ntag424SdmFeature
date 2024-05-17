0c07e1889d7203e055bab8f134b4ffad6c435510
michaelfehr <michael_fehrgmx.de>
2024-05-17 10:48:59 +0200

    update

--- app/src/main/java/de/androidcrypto/ntag424sunfeature/MainActivity.java
+++ app/src/main/java/de/androidcrypto/ntag424sunfeature/MainActivity.java
@@ -1,14 +1,37 @@
 package de.androidcrypto.ntag424sunfeature;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button menu1Prepare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +43,83 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+
+        menu1Prepare = findViewById(R.id.btnMenu1Prepare);
+
+
+        menu1Prepare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "menu1Prepare");
+                Intent intent = new Intent(MainActivity.this, PrepareActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    /**
+     * section for options menu
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+
+        MenuItem mApplications = menu.findItem(R.id.action_applications);
+        mApplications.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                //llApplicationHandling.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+
+        MenuItem mStandardFile = menu.findItem(R.id.action_standard_file);
+        mStandardFile.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                //llStandardFile.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+
+        MenuItem mExportTextFile = menu.findItem(R.id.action_export_text_file);
+        mExportTextFile.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //Log.i(TAG, "mExportTextFile");
+                //exportTextFile();
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /*
+    public void showDialog(Activity activity, String msg) {
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.logdata);
+        TextView text = dialog.findViewById(R.id.tvLogData);
+        //text.setMovementMethod(new ScrollingMovementMethod());
+        text.setText(msg);
+        Button dialogButton = dialog.findViewById(R.id.btnLogDataOk);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+     */
 }
